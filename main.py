f0629f794ba5e7f15722b6a1f711a6d85d5f093a
omrimas <omomri@gmail.com>
2019-03-18 19:19:43 +0200

    process multiple files

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import unicodedata
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
-speaker = "Clinton"
+speakers = ["Clinton"]
 corpus_name = "Clinton-Trump Corpus"
-corpus = os.path.join("data", corpus_name, speaker)
+# corpus = os.path.join("data", corpus_name, speaker)
 target_vocabulary = []
 
 
@@ -20,6 +20,7 @@ def unicodeToAscii(s):
         c for c in unicodedata.normalize('NFD', s)
         if unicodedata.category(c) != 'Mn'
     )
+
 
 # Lowercase, trim, and remove non-letter characters
 def normalizeString(s):
@@ -31,35 +32,21 @@ def normalizeString(s):
     s = re.sub(r"\s+", r" ", s).strip()
     return s
 
+
 ################################################################
 
-class LSTMGenerator(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size):
-        super(LSTMGenerator, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+# Default word tokens
+PAD_token = 0  # Used for padding short sentences
+EOS_token = 1  # End-of-sentence token
 
-        # The LSTM takes word embeddings as inputs, and outputs hidden states
-        # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-
-        # The linear layer that maps from hidden state space to vocabulary space
-        self.hidden2word = nn.Linear(hidden_dim, vocab_size)
-
-    def forward(self, sentence):
-        embeds = self.word_embeddings(sentence)
-        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
-        voc_space = self.hidden2word(lstm_out.view(len(sentence), -1))
-        word_scores = F.log_softmax(voc_space, dim=1)
-        return word_scores
 
 # Vocabulary class
 class Voc:
     def __init__(self, name):
         self.name = name
         self.word2index = {}
-        self.index2word = {}
-        self.num_words = 0
+        self.index2word = {PAD_token: "PAD", EOS_token: "EOS"}
+        self.num_words = 2
 
     def addFile(self, filePath):
         with open(filePath, 'r') as f:
@@ -74,65 +61,91 @@ class Voc:
             self.index2word[self.num_words] = word
             self.num_words += 1
 
-def text2Seq(filePath):
-    with open(os.path.join(corpus, filePath), 'r') as f:
+
+class LSTMGenerator(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size):
+        super(LSTMGenerator, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, 2)
+
+        # The linear layer that maps from hidden state space to vocabulary space
+        self.hidden2word = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, word, hidden=None):
+        embeds = self.word_embeddings(word)
+
+        rnn_output, hidden = self.lstm(embeds.view(1, 1, -1), hidden)
+        voc_space = self.hidden2word(rnn_output)
+        word_scores = F.softmax(voc_space, dim=2)
+        return word_scores, hidden
+
+
+def getSpeech(filePath):
+    with open(filePath, 'r') as f:
         seqs = [normalizeString(line) for line in f]
         seqs = list(filter(lambda x: len(x) > 0, seqs))
     return ' '.join(seqs).split(' ')
 
-def prepareSequence(seq, word_to_ix):
-    idxs = [word_to_ix[w] for w in seq]
-    return torch.tensor(idxs, dtype=torch.long)
+
+def speech2indices(seq, word_to_ix):
+    return [word_to_ix[w] for w in seq]
+
+
+def getAllFiles(corpus_name, speakers):
+    all_speech_files = []
+    for speaker in speakers:
+        speaker_path = os.path.join("data", corpus_name, speaker)
+        speaker_files = [os.path.join(speaker_path, speaker_file) for speaker_file in os.listdir(speaker_path)]
+        all_speech_files += speaker_files
+    return all_speech_files
+
+
+all_speech_files = getAllFiles(corpus_name, speakers)
 
 # prepare data for training
 voc = Voc(corpus_name)
-voc.addFile(os.path.join(corpus, "Clinton_2016-07-28.txt"))
-# training_data = text2Seq("Clinton_2016-07-28.txt")
-files = ["Clinton_2016-07-28.txt"]
+for speech_file in all_speech_files:
+    voc.addFile(speech_file)
 
-
-model = LSTMGenerator(50, 64, voc.num_words)
+model = LSTMGenerator(128, 128, voc.num_words)
+model.to(device)
 loss_function = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-with torch.no_grad():
-    training_data = text2Seq(files[0])
-    inputs = prepareSequence(training_data, voc.word2index)
-    word_scores = model(inputs)
-    print(word_scores)
+for epoch in range(1):
 
-for epoch in range(100):
-    for file in files:
-        # Step 1. Remember that Pytorch accumulates gradients.
-        # We need to clear them out before each instance
-        model.zero_grad()
-
-        # Step 2. Get our inputs ready for the network, that is, turn them into
-        # Tensors of word indices.
-        training_data = text2Seq(file)
-        speech_in = prepareSequence(training_data, voc.word2index)
-        targets = torch.cat((speech_in[1:], speech_in[0:1]))
-
-
-        # Step 3. Run our forward pass.
-        word_scores = model(speech_in)
-
-        # Step 4. Compute the loss, gradients, and update the parameters by
-        #  calling optimizer.step()
-        loss = loss_function(word_scores, targets)
-        loss.backward()
-        optimizer.step()
+    for file in all_speech_files[:10]:
+        print(file)
+        hidden = None
+        current_speech = getSpeech(file)
+        speech_indices = speech2indices(current_speech, voc.word2index)
+        speech_indices.append(EOS_token)
+        for i in range(len(speech_indices) - 1):
+            if i % 3 == 0:
+                hidden = None
+            current_word = torch.tensor(speech_indices[i], device=device)
+            model.zero_grad()
+            word_scores, hidden = model(current_word, hidden)
+            target = torch.tensor([speech_indices[i + 1]], device=device)
+            loss = loss_function(word_scores.view(1, -1), target)
+            loss.backward(retain_graph=True)
+            optimizer.step()
 
 with torch.no_grad():
-    training_data = ["like"]
-    inputs = prepareSequence(training_data, voc.word2index)
-    for i in range(10):
-        word_scores = model(inputs)
-        _, indices = torch.max(word_scores, 1)
+    hidden = None
+    seed = "the"
+    generated_speech = [seed]
+    input_word = torch.tensor([voc.word2index[seed]], device=device)
+
+    for i in range(100):
+        word_scores, hidden = model(input_word, hidden)
+        indices = torch.argmax(word_scores, 2)
         word_index = indices[0].item()
-        print(voc.index2word[word_index])
-        inputs = torch.LongTensor([word_index])
+        generated_speech.append(voc.index2word[word_index])
+        input_word = torch.tensor([word_index], device=device)
 
-
-# dataX = []
-# dataY = []
+    print(" ".join(generated_speech))
